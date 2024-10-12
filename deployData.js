fd04d7756d24437b9b276b32b4123e5543cee170
NephDev <vakih35@gmail.com>
2024-10-12 18:29:06 +0700

    Library App

---
+++ deployData.js
@@ -0,0 +1,13 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'Muhammad Furqon Rizqi';
+const studentId = 'FS11844837';
+const NetlifyDeployUrl = '';
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
